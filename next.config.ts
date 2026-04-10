d0e3a9f138daf04c48ee0d209735192cd9c21485
techbase2brand <techbase2brand@gmail.com>
2026-04-10 18:38:27 +0530

    fix(image.upstash.next.conf)

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,16 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: "./",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
